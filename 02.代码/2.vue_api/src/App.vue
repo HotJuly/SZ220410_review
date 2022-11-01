9e565bd2f5457dc4fbbae1884f45aebe6f894739
HotJuly <949588380@qq.com>
2022-11-01 11:56:12 +0800

    27.Vue-API(全局配置)

---
+++ 02.代码/2.vue_api/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <About/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  // 也就是说Vue会将配置对象.通过components属性将其变为对应的组件构造函数
+  components: {
+    HelloWorld
+  },
+  a:3,
+  mounted(){
+    // Object.assign(this.$data,this.$options.data())
+    // console.log('App',this.$options.a,c)
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 02.代码/2.vue_api/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+// vue文件暴露出去的是一个配置对象
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  a:2,
+  mounted(){
+    // Object.assign(this.$data,this.$options.data())
+    console.log('Hello',this.$options.a)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
