2f3e81ada4184a92f0b9fb1305a47e1c4ccc8876
HotJuly <949588380@qq.com>
2022-11-03 14:56:51 +0800

    46.Vue生命周期复习

---
+++ 02.代码/2.vue_api/src-测试Vue2的特性/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <!-- <h1>{{arr[2]}}</h1> -->
+    <h1 v-if="show1" ref="h1">h1</h1>
+    <h2 v-if="show2" ref="h2">h2</h2>
+    <h3 v-if="show3" ref="h3">h3</h3>
+    <!-- <button @click="clickHandler">修改数据</button> -->
+    <button @click="clickHandler1">修改页面</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      arr:[1,2,3],
+      show1:true,
+      show2:false,
+      show3:false
+    }
+  },
+  methods:{
+    clickHandler(){
+      // this.arr[2] = 7;
+      // this.arr.splice(2,1,7)
+      var data = [3,4,5]
+      // data.splice(2,1,7)
+      console.log(this.arr,data)
+    },
+    clickHandler1(){
+
+
+      this.show2=true;
+
+      this.$nextTick(()=>{
+        // console.log(this.$refs.h1)
+        console.log(this.$refs.h2)
+        console.log(this.$refs.h3)
+      })
+      
+      this.show3=true;
+
+    }
+  },
+  // mounted(){
+    
+  // }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- 02.代码/2.vue_api/src/App.vue
+++ 02.代码/2.vue_api/src/App.vue
@@ -1,49 +1,41 @@
 <template>
   <div id="app">
-    <!-- <h1>{{arr[2]}}</h1> -->
-    <h1 v-if="show1" ref="h1">h1</h1>
-    <h2 v-if="show2" ref="h2">h2</h2>
-    <h3 v-if="show3" ref="h3">h3</h3>
-    <!-- <button @click="clickHandler">修改数据</button> -->
-    <button @click="clickHandler1">修改页面</button>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  components: {
+    HelloWorld
+  },
   data(){
     return{
-      arr:[1,2,3],
-      show1:true,
-      show2:false,
-      show3:false
+      msg:123
     }
   },
-  methods:{
-    clickHandler(){
-      // this.arr[2] = 7;
-      // this.arr.splice(2,1,7)
-      var data = [3,4,5]
-      // data.splice(2,1,7)
-      console.log(this.arr,data)
-    },
-    clickHandler1(){
+  beforeCreate(){
+    // console.log('--------beforeCreate--------',this,this.$data,this.msg,this.$el)
+    console.log('--------App beforeCreate--------')
+  },
+  created(){
+    // console.log('--------created--------',this,this.$data,this.msg,this.$el)
+    console.log('--------App created--------')
+  },
+  beforeMount(){
+    // console.log('--------beforeMount--------',this,this.$data,this.msg,this.$el)
 
-
-      this.show2=true;
-
-      this.$nextTick(()=>{
-        // console.log(this.$refs.h1)
-        console.log(this.$refs.h2)
-        console.log(this.$refs.h3)
-      })
-      
-      this.show3=true;
-
-    }
-  }
+    // 通过以下代码打印,可以得知,beforeMount之前是不会生成VNode
+    // console.log('--------beforeMount--------',this.$vnode,this._vnode)
+    console.log('--------App beforeMount--------')
+  },
+  mounted(){
+    // console.log('--------mounted--------',this,this.$data,this.msg,this.$el)
+    console.log('--------App mounted--------')
+  },
 }
 </script>
 
